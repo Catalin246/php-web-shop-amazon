5d45f0a1ab004230c52b9967e4e93a60d2d43c98
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-12-29 12:20:50 +0100

    Add the favicon to the website.

---
+++ app/views/components/hdn-sign.php
@@ -0,0 +1,34 @@
+<div class="hdn-sign">
+    <div class="sign-in-section">
+        <button class="sign-in-button">Sign In</button>
+        <p class="start-here-paragraph">
+            New Customer?<a href="#" class="start-here-link"> Start here </a>
+        </p>
+        <hr class="custom-hr">
+    </div>
+
+    <div class="hdn-table">
+        <div>
+            <h3>Your Lists</h3>
+            <ul>
+                <li>Create a List</li>
+                <li>Find a List & Registry</li>
+                <li>Amazon Smile Charity Lists</li>
+            </ul>
+        </div>
+        <div class="hdn-line"></div>
+        <div>
+            <h3>Your Account</h3>
+            <ul>
+                <li>Account</li>
+                <li>Orders</li>
+                <li>Recommendations</li>
+                <li>Browsing History</li>
+                <li>Watchlist</li>
+                <li>Subscribe & Save Items</li>
+                <li>Membership</li>
+                <li>Music Library</li>
+            </ul>
+        </div>
+    </div>
+</div>
--- app/views/components/header.php
+++ app/views/components/header.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
     <title>Web Shop Amazon</title>
 </head>
 
@@ -71,40 +72,9 @@
         <i class="fa-solid fa-xmark"></i>
     </div>
     <div class="triangle"><i class="fa-solid fa-triangle"></i></div>
-    <div class="hdn-sign">
-        <div class="sign-in-section">
-            <button class="sign-in-button">Sign In</button>
-            <p class="start-here-paragraph">
-                New Customer?<a href="#" class="start-here-link"> Start here </a>
-            </p>
-            <hr class="custom-hr">
-        </div>
-
-        <div class="hdn-table">
-            <div>
-                <h3>Your Lists</h3>
-                <ul>
-                    <li>Create a List</li>
-                    <li>Find a List & Registry</li>
-                    <li>Amazon Smile Charity Lists</li>
-                </ul>
-            </div>
-            <div class="hdn-line"></div>
-            <div>
-                <h3>Your Account</h3>
-                <ul>
-                    <li>Account</li>
-                    <li>Orders</li>
-                    <li>Recommendations</li>
-                    <li>Browsing History</li>
-                    <li>Watchlist</li>
-                    <li>Subscribe & Save Items</li>
-                    <li>Membership</li>
-                    <li>Music Library</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <?php
+    include __DIR__ . '/../components/hdn-sign.php';
+    ?>
     <div class="black"></div>
     <header>
         <div class="first">
