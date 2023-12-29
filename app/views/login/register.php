a134a5f086f446a07f1bc5ba12f90641e48a0a90
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-12-29 13:51:44 +0100

    Create the sign in and register frontend.

--- app/views/login/register.php
+++ app/views/login/register.php
@@ -1,1 +1,70 @@
-This is the register page.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <title>Register</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <a href="/"><img class="small-logo" src="../images/amazon-logo.png" alt=""></a>
+        </div>
+
+        <div class="row">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <form method="POST" action="#" role="form">
+                        <div class="form-group">
+                            <h2>Create Account</h2>
+                        </div>
+                        <div class="form-group">
+                            <strong>Your Name</strong>
+                            <input id="registerName" type="text" maxlength="50" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <strong>Email</strong>
+                            <input id="registerEmail" type="email" maxlength="50" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <strong>Password</strong>
+                            <input id="registerPassword" type="password" maxlength="25" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <strong>Re-enter Password</strong>
+                            <input id="registerPasswordConfirm" type="password" maxlength="25" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding-top: 12px;">
+                            <button id="registerSubmit" type="submit" class="btn sign-in-button btn-block">Create your
+                                Amazon account</button>
+                        </div>
+                        <div class="form-group divider">
+                            <hr><small>Already have an account?</small>
+                        </div>
+                        <p class="form-group"><a href="signin" class="btn sign-in-button btn-block">Sign in to your
+                                account</a>
+                        </p>
+                        <p class="form-group">By creating an account you agree to our <a class="link"
+                                href="#">Conditions
+                                of
+                                Use</a> and <a class="link" href="#">Privacy Notice</a>.
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- app/views/login/sign-in.php
+++ app/views/login/sign-in.php
@@ -1,1 +1,61 @@
-This is the sign in page.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <title>Sign In</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <a href="/"><img class="small-logo" src="../images/amazon-logo.png" alt=""></a>
+        </div>
+
+        <div class="row">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <form method="POST" action="#" role="form">
+                        <div class="form-group">
+                            <h2>Sign in</h2>
+                        </div>
+                        <div class="form-group">
+                            <strong>Email or mobile phone number</strong>
+                            <input id="signinEmail" type="email" maxlength="50" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <strong>Password</strong>
+                            <span class="right"><a class="link" href="#">Forgot your password?</a></span>
+                            <input id="signinPassword" type="password" maxlength="25" class="form-control">
+                        </div>
+                        <div class="form-group" style="padding-top: 12px;">
+                            <button id="signinSubmit" type="submit" class="btn sign-in-button btn-block">Sign
+                                in</button>
+                        </div>
+                        <div class="form-group divider">
+                            <hr class="left"><small>New to site?</small>
+                            <hr class="right">
+                        </div>
+                        <p class="form-group"><a href="register" class="btn sign-in-button btn-block">Create an
+                                account</a>
+                        </p>
+                        <p class="form-group">By signing in you are agreeing to our <a class="link" href="#">Terms of
+                                Use</a> and our
+                            <a class="link" href="#">Privacy Policy</a>.
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
