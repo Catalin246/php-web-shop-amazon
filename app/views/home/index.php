ce5ff016bf4a9fd91bcb192fd3964adea67ea85f
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-12-30 13:50:57 +0100

    Remove extra components from the home page.

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -38,36 +38,6 @@ include __DIR__ . '/../components/header.php';
                     <a href="#">See More</a>
                 </div>
             </div>
-            <div class="product-comp">
-                <div class="box box-c">
-                    <h3>Shop Valentine's Day</h3>
-                    <div>
-                        <img src="assets/images/shop valentine's.jpg" alt="">
-                    </div>
-                    <a href="#">See More</a>
-                </div>
-                <div class="box box-c">
-                    <h3>Electronics</h3>
-                    <div>
-                        <img src="assets/images/electronics.jpg" alt="">
-                    </div>
-                    <a href="#">See More</a>
-                </div>
-                <div class="box box-c">
-                    <h3>Find your ideal TV</h3>
-                    <div>
-                        <img src="assets/images/find your deal.jpg" alt="">
-                    </div>
-                    <a href="#">See More</a>
-                </div>
-                <div class="box box-c">
-                    <h3>Easy Returns</h3>
-                    <div>
-                        <img src="assets/images/easy return.jpg" alt="">
-                    </div>
-                    <a href="#">See More</a>
-                </div>
-            </div>
         </div>
         <div class="slider">
             <div class="image-box">
@@ -87,33 +57,8 @@ include __DIR__ . '/../components/header.php';
         </div>
     </div>
 </section>
+
 <section class="sec-2">
-    <h3>Popular Products in Wireless Internationally</h3>
-    <div class="slide-sec">
-        <div class="l-btn"><i class="fa-solid fa-chevron-left"></i></div>
-        <div class="r-btn"><i class="fa-solid fa-chevron-right"></i></div>
-        <h3>Popular Products in Wireless Internationally</h3>
-        <ul class="product-slide">
-            <li><img src="assets/images/pinter1.jpg" alt=""></li>
-            <li><img src="assets/images/pinter2.jpg" alt=""></li>
-            <li><img src="assets/images/pinter3.jpg" alt=""></li>
-            <li><img src="assets/images/pinter4.jpg" alt=""></li>
-            <li><img src="assets/images/pinter5.jpg" alt=""></li>
-            <li><img src="assets/images/pinter6.jpg" alt=""></li>
-            <li><img src="assets/images/pinter7.jpg" alt=""></li>
-            <li><img src="assets/images/pinter8.jpg" alt=""></li>
-            <li><img src="assets/images/pinter9.jpg" alt=""></li>
-            <li><img src="assets/images/pinter10.jpg" alt=""></li>
-            <li><img src="assets/images/pinter11.jpg" alt=""></li>
-            <li><img src="assets/images/pinter12.jpg" alt=""></li>
-            <li><img src="assets/images/pinter13.jpg" alt=""></li>
-            <li><img src="assets/images/pinter14.jpg" alt=""></li>
-            <li><img src="assets/images/pinter15.jpg" alt=""></li>
-        </ul>
-    </div>
-</section>
-<section class="sec-2">
-    <!-- <h3>Popular Products in Wirelessly Internationally</h3> -->
     <div class="slide-sec">
         <div class="l-btn btn-1b"><i class="fa-solid fa-chevron-left"></i></div>
         <div class="r-btn btn-1a"><i class="fa-solid fa-chevron-right"></i></div>
@@ -136,67 +81,7 @@ include __DIR__ . '/../components/header.php';
         </ul>
     </div>
 </section>
-<div class="product-comp">
-    <div class="box box-c">
-        <h3>Shop Activity Trackers and Smart Watches</h3>
-        <div>
-            <img src="assets/images/watch.jpg" alt="">
-        </div>
-        <a href="#">See More</a>
-    </div>
-    <div class="box box-d">
-        <h3>Comfy Styles for Men</h3>
-        <div class="box-a">
-            <div>
-                <img src="assets/images/cm1.jpg" alt="">
-                <span>Sweatshirts</span>
-            </div>
-            <div>
-                <img src="assets/images/cm2.jpg" alt="">
-                <span>Joggers</span>
-            </div>
-            <div>
-                <img src="assets/images/cm3.jpg" alt="">
-                <span>Cardigans</span>
-            </div>
-            <div>
-                <img src="assets/images/cm4.jpg" alt="">
-                <span>Easy Tees</span>
-            </div>
-        </div>
-        <a href="#">See More</a>
-    </div>
-    <div class="box box-d">
-        <h3>Gaming Merchandise</h3>
-        <div class="box-a">
-            <div>
-                <img src="assets/images/gm1.jpg" alt="">
-                <span>Apparel</span>
-            </div>
-            <div>
-                <img src="assets/images/gm2.jpg" alt="">
-                <span>Hats</span>
-            </div>
-            <div>
-                <img src="assets/images/gm3.jpg" alt="">
-                <span>Action Figures</span>
-            </div>
-            <div>
-                <img src="assets/images/gm4.jpg" alt="">
-                <span>Mugs</span>
-            </div>
-        </div>
-        <a href="#">See More</a>
-    </div>
-    <div class="box box-c">
-        <h3>Shop Pet Supplies</h3>
-        <div>
-            <img src="assets/images/pets.jpg" alt="">
-        </div>
-        <a href="#">See More</a>
-    </div>
 
-</div>
 <div class="product-comp">
     <div class="box box-c">
         <h3>For your Fitness Needs</h3>
@@ -228,7 +113,6 @@ include __DIR__ . '/../components/header.php';
     </div>
 </div>
 <section class="sec-2">
-    <!-- <h3>Popular Products in Wirelessly Internationally</h3> -->
     <div class="slide-sec">
         <div class="l-btn btn-1c"><i class="fa-solid fa-chevron-left"></i></div>
         <div class="r-btn btn-1d"><i class="fa-solid fa-chevron-right"></i></div>
