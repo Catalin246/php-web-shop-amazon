4da46f0594c78a470acf294a51cf7c011d4f04e9
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-01-16 22:40:51 +0100

    Add a back button on the index page.

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -6,7 +6,10 @@ include __DIR__ . '/../components/admin-header.php';
 
 <div class="container p-5">
     <div class="row d-flex justify-content-center">
-        <div class="col-10 py-4 d-flex justify-content-end">
+        <div class="col-5 py-4 d-flex justify-content-start">
+            <a href="/"><button type="button" class="btn btn-danger btn-sm">Back</button></a>
+        </div>
+        <div class="col-5 py-4 d-flex justify-content-end">
             <a href="/user/add"><button type="button" class="btn btn-primary btn-sm">Add
                     User</button></a>
         </div>
