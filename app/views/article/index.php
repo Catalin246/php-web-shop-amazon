acf342d46b591642018ed717d33e61abce61ee25
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-01-21 12:57:02 +0100

    Create the frontend for the article panel page.

--- app/views/article/index.php
+++ app/views/article/index.php
@@ -4,27 +4,30 @@ include __DIR__ . '/../components/admin-header.php';
 
 <script src="../dist/user.bundle.js"></script>
 
-<!-- <div class="container p-5">
+<div class="container p-5">
     <div class="row d-flex justify-content-center">
         <div class="col-5 py-4 d-flex justify-content-start">
             <a href="/admin"><button type="button" class="btn btn-danger btn-sm">Back</button></a>
         </div>
         <div class="col-5 py-4 d-flex justify-content-end">
-            <a href="/user/add"><button type="button" class="btn btn-primary btn-sm">Add
-                    User</button></a>
+            <a href="/article/add"><button type="button" class="btn btn-primary btn-sm">Add
+                    Article</button></a>
         </div>
     </div>
     <div class="row d-flex justify-content-center">
         <div class="col-10">
-            <table class="table table-bordered" id="userTable">
+            <table class="table table-bordered" id="articleTable">
                 <thead>
                     <tr>
                         <th scope="col" class="col-2">ID</th>
-                        <th scope="col" class="col-2">Email</th>
+                        <th scope="col" class="col-2">Category ID</th>
                         <th scope="col" class="col-2">Name</th>
-                        <th scope="col" class="col-2">Phone</th>
-                        <th scope="col" class="col-2">Role</th>
-                        <th scope="col" class="col-2">Actions</th>
+                        <th scope="col" class="col-2">Description</th>
+                        <th scope="col" class="col-2">Image</th>
+                        <th scope="col" class="col-2">Price</th>
+                        <th scope="col" class="col-2">Num Sales</th>
+                        <th scope="col" class="col-2">Amount</th>
+                        <th scope="col" class="col-2">Display</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -32,4 +35,4 @@ include __DIR__ . '/../components/admin-header.php';
             </table>
         </div>
     </div>
-</div> -->
+</div>
