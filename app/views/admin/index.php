6300eaf385a2fc58f0b58504cbcffa9168ce6ae8
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-01-21 13:59:39 +0100

    Do not display the category panel.

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -29,20 +29,20 @@ include __DIR__ . '/../components/admin-header.php';
         <div class="col-md-6 p-3">
             <div class="card">
                 <div class="card-body">
-                    <i class="fas fa-tags fa-4x text-black"></i>
-                    <h5 class="card-title pt-2">Categories</h5>
-                    <a href="/category" class="card-text card-link" class="">Go to Categories panel.</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 p-3">
-            <div class="card">
-                <div class="card-body">
                     <i class="fas fa-shopping-cart fa-4x text-black"></i>
                     <h5 class="card-title pt-2">Orders</h5>
                     <a href="/order" class="card-text card-link" class="">Go to Orders panel.</a>
                 </div>
             </div>
+        </div>
+        <div class="col-md-6 p-3">
+            <!-- <div class="card">
+                <div class="card-body">
+                    <i class="fas fa-tags fa-4x text-black"></i>
+                    <h5 class="card-title pt-2">Categories</h5>
+                    <a href="/category" class="card-text card-link" class="">Go to Categories panel.</a>
+                </div>
+            </div> -->
         </div>
     </div>
 </div>
