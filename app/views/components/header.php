8ab3e637960143b5e1e6bf941ce7f19fbb48929c
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-03-25 20:26:25 +0100

    Remove header links.

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -11,9 +11,6 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="icon" type="image/x-icon" href="../assets/images/favicon.png">
     <title>Web Shop Amazon</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
 </head>
 
 <body>
