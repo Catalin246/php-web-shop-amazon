11cb940e66f72c9192748839de6826b09f4e492e
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-03-28 20:08:35 +0100

    Add some things on the category page

--- app/views/home/category.php
+++ app/views/home/category.php
@@ -2,7 +2,71 @@
 include __DIR__ . '/../components/header.php';
 ?>
 
-This will display the products per categoy.
+<div class="flex">
+    TO DO
+    <!-- Sidebar Filter Section -->
+    <!-- <div class=" margin-right"> -->
+        <!-- <h2>Filter By</h2> -->
+
+        <!-- Category Filter -->
+        <!-- <div class="filter-group">
+            <h3>Category</h3>
+            <ul>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> Electronics
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> Clothing
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> Home
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> Books
+                    </label>
+                </li>
+            </ul>
+        </div> -->
+
+        <!-- Price Filter -->
+        <!-- <div class="filter-group">
+            <h3>Price Range</h3>
+            <ul>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> $0 - $50
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> $51 - $100
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> $101 - $200
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input type="radio" name="category" value="electronics"> $200+
+                    </label>
+                </li>
+            </ul>
+        </div>
+    </div> -->
+
+    <!-- Articles Section -->
+    <!-- <div class="articles">
+    </div> -->
+</div>
 
 <script src="../dist/home.bundle.js"></script>
 
