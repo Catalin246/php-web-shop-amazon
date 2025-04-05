b2cef5079bfa03ddc55bb43f9e42e1b5603e6a0c
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-04-05 13:07:27 +0200

    Redirect users to the payment page.

--- app/controllers/paymentcontroller.php
+++ app/controllers/paymentcontroller.php
@@ -6,14 +6,14 @@ class PaymentController
 {
     function __construct()
     {
-        // Check if user is logged in
-        if (!isset($_SESSION['user'])) {
-            // Not logged in, set a session flag to show a login required message
-            $_SESSION['login_required'] = true;
-            // Redirect to login page
-            header("Location: /login");
-            exit();
-        }
+        // // Check if user is logged in
+        // if (!isset($_SESSION['user'])) {
+        //     // Not logged in, set a session flag to show a login required message
+        //     $_SESSION['login_required'] = true;
+        //     // Redirect to login page
+        //     header("Location: /login");
+        //     exit();
+        // }
     }
 
     public function index()
